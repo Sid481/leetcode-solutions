527075188972398367c4200446e175b76e0328b4
Pradip Trimbak Ghadage <pradip.ghadage@hotmail.com>
2025-07-26 18:59:24 +0530

    Time: 1 ms (99.92%), Space: 44.6 MB (81.27%) - LeetHub

--- 0039-combination-sum/0039-combination-sum.java
+++ 0039-combination-sum/0039-combination-sum.java
@@ -5,8 +5,8 @@ class Solution {
         backtrack(candidates,target,0,new ArrayList<>(),result);
         return result;
     }
-    
-    private void backtrack(int candidates[],int target,int start,List<Integer>path, List<List<Integer>>result) {
+
+    private void backtrack(int candidates[], int target, int start, List<Integer>path, List<List<Integer>>result) {
         if(target==0) {
             result.add(new ArrayList<>(path));
             return;
