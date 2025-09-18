25aec4fe5a31b3a457b5900f48344e037a7d10eb
Pradip Trimbak Ghadage <pradip.ghadage@hotmail.com>
2025-09-18 18:24:20 +0530

    Time: 13 ms (67.99%), Space: 45.8 MB (56.3%) - LeetHub

--- 0451-sort-characters-by-frequency/0451-sort-characters-by-frequency.java
+++ 0451-sort-characters-by-frequency/0451-sort-characters-by-frequency.java
@@ -1,32 +1,20 @@
 class Solution {
     public String frequencySort(String s) {
-        int maxValue = Integer.MIN_VALUE;
-        HashMap<Character,Integer> map = new HashMap<>();
+        Map<Character,Integer> map = new HashMap<>();
+        PriorityQueue<Map.Entry<Character,Integer>> maxHeap;
         for(char ch : s.toCharArray()) {
             map.put(ch, map.getOrDefault(ch,0)+1);
         }
-        for(Map.Entry<Character,Integer> entry : map.entrySet()) {
-          maxValue = Math.max(maxValue,entry.getValue());
+        maxHeap = new PriorityQueue<>((a,b)->b.getValue()-a.getValue());
+        maxHeap.addAll(map.entrySet());
+        StringBuilder sb = new StringBuilder();
+        while(!maxHeap.isEmpty()) {
+           char ch =  maxHeap.peek().getKey();
+           int count = maxHeap.poll().getValue();
+           for(int i=0; i<count; i++) {
+            sb.append(ch);
+           }
         }
-       List<Character>[] bucket = new List[maxValue+1];
-
-       for(Map.Entry<Character,Integer> entry : map.entrySet()) {
-        int freq = entry.getValue();
-        char ch = entry.getKey();
-        if(bucket[freq]==null) {
-            bucket[freq] = new ArrayList<>();
-
-        }
-        bucket[freq].add(ch);
-       }
-       StringBuilder sb = new StringBuilder();
-       for(int i=maxValue; i>=0; i--) {
-        if(bucket[i]!=null) {
-            for(char ch :  bucket[i]) { 
-                sb.append(String.valueOf(ch).repeat(i));
-                }
-            }
-       }
        return sb.toString();
     }
 }
