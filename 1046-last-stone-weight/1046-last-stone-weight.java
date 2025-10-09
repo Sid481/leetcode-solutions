115aaa98b2588ec276ff1d616f940b0f4a741ff6
Pradip Trimbak Ghadage <pradip.ghadage@hotmail.com>
2025-10-09 17:45:30 +0530

    Time: 1 ms (98.68%), Space: 41.3 MB (45.09%) - LeetHub

---
+++ 1046-last-stone-weight/1046-last-stone-weight.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int lastStoneWeight(int[] stones) {
+        if(stones.length==1) return stones[0];
+        PriorityQueue<Integer> maxHeap = new PriorityQueue<>((a,b)-> b-a);
+        for(int stone : stones) {
+            maxHeap.offer(stone);
+        }
+        while(maxHeap.size()>1) {
+            Integer first = maxHeap.poll();
+            Integer second = maxHeap.poll();
+            if(Math.abs(first-second)>0)
+            maxHeap.offer(Math.abs(first-second));
+        }
+        return maxHeap.size()>0?maxHeap.peek():0;
+    }
+}
