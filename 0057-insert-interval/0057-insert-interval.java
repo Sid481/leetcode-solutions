9da7b43863d15a596481c606205a5ada32d7b60a
Pradip Trimbak Ghadage <pradip.ghadage@hotmail.com>
2025-10-16 19:56:24 +0530

    Time: 4 ms (8.02%), Space: 45.7 MB (8.24%) - LeetHub

--- 0057-insert-interval/0057-insert-interval.java
+++ 0057-insert-interval/0057-insert-interval.java
@@ -1,36 +1,22 @@
 class Solution {
     public int[][] insert(int[][] intervals, int[] newInterval) {
+        PriorityQueue<int[]>minHeap = new PriorityQueue<>((a,b)->Integer.compare(a[0],b[0]));
         List<int[]> result = new ArrayList<>();
-        int start = newInterval[0];
-        int end = newInterval[1];
-        PriorityQueue<int[]> minHeap = new PriorityQueue<>((a,b)->Integer.compare(a[0],b[0]));
-        int count = 0;
-        int newStart = 0;
         for(int[] interval : intervals) {
-                if(end >= interval[0] && count==0) {
-                    int merged[] = new int[]{Math.min(start,interval[0]),Math.max(end,interval[1])};
-                    end = merged[1];
-                    newStart = Math.min(start,interval[0]);
-                    count++;
-                    minHeap.offer(merged);
-                } else if(end >= interval[0]) {
-                    int top [] = minHeap.poll();
-                    int merged[] = new int[]{Math.min(interval[0],top[0]),Math.max(end,interval[1])};
-                    end = merged[1];
-                    minHeap.offer(merged);
-                } else {
-                    minHeap.add(interval);
-                }
-    }
-    if(count==0) {
+            minHeap.offer(interval);
+        }
         minHeap.offer(newInterval);
-    }
-    while(!minHeap.isEmpty()) {
-        result.add(minHeap.poll());
-    }
-    
-    int[][] finalResult = result.toArray(new int[result.size()][]);
-    Arrays.sort(finalResult, (a,b)->Integer.compare(a[0],b[0]));
-    return finalResult;
+        int current[] = minHeap.poll();
+        while(!minHeap.isEmpty()) {
+            int[] next = minHeap.poll();
+            if(current[1]>=next[0]) {
+                current[1] = Math.max(current[1],next[1]);
+            } else {
+                result.add(current);
+                current = next;
+            }
+        }
+        result.add(current);
+        return result.toArray(new int[result.size()][]);
     }
 }
