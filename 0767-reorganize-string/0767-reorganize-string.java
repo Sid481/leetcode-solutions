283c5b8a607e9885963b02e2ecb60ecbe3a44042
Pradip Trimbak Ghadage <pradip.ghadage@hotmail.com>
2025-10-19 18:33:38 +0530

    Time: 5 ms (37.52%), Space: 42.2 MB (13.66%) - LeetHub

---
+++ 0767-reorganize-string/0767-reorganize-string.java
@@ -0,0 +1,31 @@
+class Solution {
+    public String reorganizeString(String s) {
+        Map<Character,Integer>map = new HashMap<>();
+
+        for(char ch : s.toCharArray()) {
+            map.put(ch,map.getOrDefault(ch,0)+1);
+        }
+        for(int size : map.values()) {
+            if(size>(s.length()+1/2)) return "";
+        }
+
+        PriorityQueue<Map.Entry<Character,Integer>> maxHeap = new PriorityQueue<>((a,b)->
+        b.getValue()-a.getValue());
+        
+        maxHeap.addAll(map.entrySet());
+        StringBuilder sb = new StringBuilder();
+        Map.Entry<Character,Integer> prev = null;
+
+        while(!maxHeap.isEmpty()) {
+            Map.Entry<Character,Integer> current = maxHeap.poll();
+            sb.append(current.getKey());
+            current.setValue(current.getValue()-1);
+            if(prev!=null && prev.getValue()>0) {
+                maxHeap.offer(prev);
+            }
+            prev = current;
+        }
+        if(sb.length()!=s.length()) return "";
+        return sb.toString();
+    }
+}
