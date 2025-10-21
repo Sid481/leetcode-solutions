b6bca6f8e037ddf71137f7b27bc4339f7fd52dca
Pradip Trimbak Ghadage <pradip.ghadage@hotmail.com>
2025-10-21 18:07:02 +0530

    Time: 34 ms (30.78%), Space: 88.4 MB (93.72%) - LeetHub

--- 0380-insert-delete-getrandom-o1/0380-insert-delete-getrandom-o1.java
+++ 0380-insert-delete-getrandom-o1/0380-insert-delete-getrandom-o1.java
@@ -25,9 +25,8 @@ class RandomizedSet {
     }
     
     public int getRandom() {
-        //Random random = new Random();
-        //return list.get(random.nextInt(list.size()));
-        return list.get((int)(Math.random()*list.size()));
+        Random random = new Random();
+        return list.get(random.nextInt(list.size()));
     }
 }
 
