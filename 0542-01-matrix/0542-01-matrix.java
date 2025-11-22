e31d02a98eb10428deeb56affbaf00e347d723e0
Pradip Trimbak Ghadage <pradip.ghadage@hotmail.com>
2025-11-22 16:59:25 +0530

    Time: 15 ms (38.01%), Space: 49.1 MB (17.77%) - LeetHub

--- 0542-01-matrix/0542-01-matrix.java
+++ 0542-01-matrix/0542-01-matrix.java
@@ -5,36 +5,33 @@ class Solution {
         int col = mat[0].length;
         int result[][] = new int[row][col];
         Queue<int[]> queue = new LinkedList<>();
+        boolean[][] visited = new boolean[row][col];
         for(int i=0; i<row; i++) {
             for(int j=0; j<col; j++) {
-                if(mat[i][j]==1)
+                if(mat[i][j]==0) {
                 queue.offer(new int[]{i,j});
+                visited[i][j] = true;
+                }
             }
         }
-        for(int i=0; i<row; i++) {
-            for(int j=0; j<col; j++) {
-                result[i][j] = 0;
-            }
-        }
+       int distance = 0;
        while(!queue.isEmpty()) {
         int size = queue.size();
         for(int i=0; i<size; i++) {
             int[] curr = queue.poll();
-            int minutes=0;
+            result[curr[0]][curr[1]] = distance;
             for(int[] dir : direction) {
                 int nr = curr[0]+dir[0];
                 int nc = curr[1]+dir[1];
-                if(nr<0 || nc<0 || nr>=row || nc>=col)continue;
-                if(mat[nr][nc]==0) {
-                    minutes++;
-                    result[curr[0]][curr[1]] = minutes;
-                    return result;
-                } else {
+
+                if(nr>=0 && nc>=0 && nr<row && nc<col && !visited[nr][nc] && mat[nr][nc]==1) {
+                    visited[nr][nc] = true;
                     queue.offer(new int[]{nr,nc});
-                    }
-                }   
+                    } 
+                }  
             }
-        }
+            distance++; 
+       }
         return result;
     }
 }
