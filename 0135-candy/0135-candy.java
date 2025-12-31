aaddb439dff7a8e57d33bac470e9245483af205c
Pradip Trimbak Ghadage <pradip.ghadage@hotmail.com>
2025-12-31 19:58:36 +0530

    Time: 3 ms (88.92%), Space: 47.8 MB (16.52%) - LeetHub

---
+++ 0135-candy/0135-candy.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int candy(int[] ratings) {
+        int[]child = new int[ratings.length];
+
+        for(int i=0; i<child.length; i++)
+            child[i]=1;
+        for(int left = 1; left<child.length; left++) {
+            if(ratings[left]>ratings[left-1])
+            child[left] = child[left-1]+1;
+        }
+        for(int right=child.length-2; right>=0; right--) {
+            if(ratings[right]>ratings[right+1]) 
+            child[right] =Math.max(child[right],child[right+1]+1);
+        }
+        int numberOfCandies = 0;
+        for(int i=0; i<child.length; i++) {
+            numberOfCandies += child[i];
+        }
+        return numberOfCandies;
+    }
+}
