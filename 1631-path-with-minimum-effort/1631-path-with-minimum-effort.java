9ffe7f6a82a4e5c2f5942782f24bbdd64f9de970
Pradip Trimbak Ghadage <pradip.ghadage@hotmail.com>
2026-01-27 17:58:16 +0530

    Time: 42 ms (58.97%), Space: 47.9 MB (67.58%) - LeetHub

--- 1631-path-with-minimum-effort/1631-path-with-minimum-effort.java
+++ 1631-path-with-minimum-effort/1631-path-with-minimum-effort.java
@@ -2,34 +2,34 @@ class Solution {
     public int minimumEffortPath(int[][] heights) {
         int row = heights.length;
         int column = heights[0].length;
-        int direction[][] = {{0,1},{0,-1},{1,0},{-1,0}};
-        int[][] dist = new int[row][column];
-        for(int[] ro : dist) {
-            Arrays.fill(ro,Integer.MAX_VALUE);
+        int direction[][] = {{1,0},{0,1},{-1,0},{0,-1}};
+        PriorityQueue<int[]>minHeap = new PriorityQueue<>((a,b)->(a[0]-b[0]));
+        int[][] distance = new int[row][column];
+        for(int[]ro:distance) {
+        Arrays.fill(ro, Integer.MAX_VALUE);
         }
-        dist[0][0] = 0;
-        PriorityQueue<int[]> minHeap = new PriorityQueue<>((a,b)->a[0]-b[0]);
-        minHeap.offer(new int[]{0,0,0});
-
+        distance[0][0] = 0;
+        minHeap.add(new int[]{0,0,0});
         while(!minHeap.isEmpty()) {
-           int[] curr = minHeap.poll();
-           int diff = curr[0];
-           int r = curr[1];
-           int c = curr[2];
-           if(r==row-1 && c==column-1) return diff;
-           if(dist[r][c]<diff) continue;
-
-           for(int[] dir : direction) {
-            int newR = r + dir[0];
-            int newC = c + dir[1];
-            if(newR<0 || newC<0 || newR>=row || newC>=column) continue;
-            int newDiff = Math.max(diff, Math.abs(heights[r][c]-heights[newR][newC]));
-            if(newDiff<dist[newR][newC]) {
-                dist[newR][newC] = newDiff;
+            int[] curr = minHeap.poll();
+            int diff = curr[0];
+            int r = curr[1];
+            int c = curr[2];
+            if(r==row-1 && c==column-1)return diff;
+            if(diff > distance[r][c]) continue;
+            for(int dir[] : direction) {
+                int newR = r + dir[0];
+                int newC = c + dir[1];
+                if(newR<0 || newC<0 || newR>=row || newC>=column) continue;
+                int newDiff = Math.max(diff,Math.abs(heights[r][c]-heights[newR][newC]));
+                if(newDiff<distance[newR][newC]) {
+                distance[newR][newC] = newDiff;
                 minHeap.offer(new int[]{newDiff,newR,newC});
+                }
+
             }
-           }
-        }
-        return 0;
+
+         }
+         return 0;
     }
 }
