d26cd7ee1925e5b3961fd68a6e991c28fa200d1c
Pradip Trimbak Ghadage <pradip.ghadage@hotmail.com>
2026-01-28 16:23:51 +0530

    Time: 12 ms (56.22%), Space: 46.5 MB (39.84%) - LeetHub

--- 0778-swim-in-rising-water/0778-swim-in-rising-water.java
+++ 0778-swim-in-rising-water/0778-swim-in-rising-water.java
@@ -1,27 +1,28 @@
 class Solution {
     public int swimInWater(int[][] grid) {
-        PriorityQueue<int[]> maxHeap = new PriorityQueue<>((a,b)->a[0]-b[0]);
-        int[][] directions = new int[][]{{-1,0},{1,0},{0,1},{0,-1}};
+        int[][] direction = new int[][] {{1,0},{0,1},{-1,0},{0,-1}};
+        PriorityQueue<int[]> minHeap = new PriorityQueue<>((a, b)->a[0]-b[0]);
+        minHeap.add(new int[] {grid[0][0],0,0});
         int n = grid.length;
         boolean[][] visited = new boolean[n][n];
-        
-        maxHeap.offer(new int[]{grid[0][0],0,0});       
-        while(!maxHeap.isEmpty()) {
-            int[] current = maxHeap.poll();
+
+        while (!minHeap.isEmpty()) {
+            int current[] = minHeap.poll();
             int elevation = current[0];
             int r = current[1];
             int c = current[2];
-            if(r==n-1 && c==n-1) return elevation;
-            if(visited[r][c]) continue;
+            if (r == n - 1 && c == n - 1)
+                return elevation;
+            if (visited[r][c])
+                continue;
             visited[r][c] = true;
 
-            for(int[] dir : directions) {
-                int newR = r + dir[0];
-                int newC = c + dir[1];
-                if(newR>=0 && newC>=0 && newR<n && newC<n && !visited[newR][newC]) {
-                int newElevation = Math.max(elevation, grid[newR][newC]);
-                maxHeap.offer(new int[]{newElevation,newR,newC});
-                }
+            for (int[] dir : direction) {
+                int newR = dir[0] + r;
+                int newC = dir[1] + c;
+                if (newR<0 || newC<0 || newR>=n || newC>=n || visited[newR][newC]) continue;
+                    int newElevation = Math.max(elevation, grid[newR][newC]);
+                    minHeap.offer(new int[] { newElevation, newR, newC });
             }
         }
         return -1;
